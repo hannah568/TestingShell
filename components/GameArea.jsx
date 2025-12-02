0c6847b669876d3ad1ee9d1ca1b14c3863206685
hannah568 <s24113306@stu.edu.tw>
2025-12-02 15:11:54 +0800

    Add files via upload

--- components/GameArea.jsx
+++ components/GameArea.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect, useCallback, useRef } from 'react';
 import { GameStatus, Difficulty } from '../types';
 import { Cup } from './Cup';
--- types.js
+++ types.js
@@ -1,4 +1,3 @@
-
 export const GameStatus = {
   IDLE: 'IDLE',
   SHUFFLING: 'SHUFFLING',
